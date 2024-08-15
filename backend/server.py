e3a989b71f73b904dd102d0e8f83d119cd516e85
Max Hua <70117371+BleakBubbles@users.noreply.github.com>
2024-08-15 02:51:22 -0400

    Performed further tests with template matching

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_cors import CORS
 import cv2
 import numpy as np
+import tempfile
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -15,16 +16,28 @@ def analyze():
     try:
         file = request.files['image']
         npimg = np.fromfile(file, np.uint8)
-        rawimg = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-        height, width, channels = rawimg.shape
+        rawimg = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
+        rawimg_gray = cv2.cvtColor(rawimg, cv2.COLOR_BGR2GRAY)
+        height = rawimg.shape[0]
         factor = 270/height
-        img = cv2.resize(rawimg, None, fx=factor, fy=factor)
-        templ = cv2.imread('./static/items/Liquid_Valkyrie.png')
-        result = cv2.matchTemplate(img, templ, cv2.TM_CCOEFF)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-        return str(max_loc)
-    except:
-        return "An error occured."
+        img = cv2.resize(rawimg_gray, None, fx=factor, fy=factor)
+        templ = cv2.imread('backend\\static\\items\\Liquid_Valkyrie.png', 0)
+        h, w = templ.shape
+        result = cv2.matchTemplate(img, templ, cv2.TM_SQDIFF_NORMED)
+        # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        threshold = 0.1
+        loc = np.where(result <= threshold)
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), 255, 1)
+        status, buffer = cv2.imencode('.png', img)
+        data = np.array(buffer)
+        bytes = data.tobytes()
+        temp = tempfile.TemporaryFile()
+        temp.write(bytes)
+        temp.seek(0)
+        return send_file(temp, download_name="temp.png")
+    except Exception as e:
+        return str(e)
     
 if __name__ == "__main__":
     app.run(debug=True)
