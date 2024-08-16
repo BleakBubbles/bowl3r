1fbc4f16b23376bd520f3eb8686237580522734c
Max Hua <max@einfolab.com>
2024-08-16 17:38:12 -0400

    See previous commit

--- backend/server.py
+++ backend/server.py
@@ -3,9 +3,10 @@ from flask_cors import CORS
 import cv2
 import numpy as np
 import tempfile
+import os
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -14,21 +15,54 @@ def hello_world():
 @app.route("/analyze", methods=['POST'])
 def analyze():
     try:
+        all = True
         file = request.files['image']
         npimg = np.fromfile(file, np.uint8)
         rawimg = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
-        rawimg_gray = cv2.cvtColor(rawimg, cv2.COLOR_BGR2GRAY)
         height = rawimg.shape[0]
         factor = 270/height
-        img = cv2.resize(rawimg_gray, None, fx=factor, fy=factor)
-        templ = cv2.imread('backend\\static\\items\\Liquid_Valkyrie.png', 0)
-        h, w = templ.shape
-        result = cv2.matchTemplate(img, templ, cv2.TM_SQDIFF_NORMED)
-        # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-        threshold = 0.1
-        loc = np.where(result <= threshold)
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), 255, 1)
+        img = cv2.resize(rawimg, None, fx=factor, fy=factor)
+        img2 = img.copy()
+        if all:
+            for active in os.listdir('backend\\static\\actives'):
+                templ = cv2.imread(f'backend\\static\\actives\\{active}', cv2.IMREAD_UNCHANGED)
+                h, w = templ.shape[:-1]
+                alpha_channel = np.array(cv2.split(templ)[3])
+                result = cv2.matchTemplate(img2, templ, cv2.TM_SQDIFF_NORMED, mask=alpha_channel)
+                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+                if (min_val <= 0.1):
+                    print(active)
+                    color = (0, 0, 255, 255)
+                    cv2.rectangle(img, min_loc, (min_loc[0] + w, min_loc[1] + h), color, 2)
+            for gun in os.listdir('backend\\static\\guns'):
+                templ = cv2.imread(f'backend\\static\\guns\\{gun}', cv2.IMREAD_UNCHANGED)
+                h, w = templ.shape[:-1]
+                alpha_channel = np.array(cv2.split(templ)[3])
+                result = cv2.matchTemplate(img2, templ, cv2.TM_SQDIFF_NORMED, mask=alpha_channel)
+                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+                if (min_val <= 0.1):
+                    print(gun)
+                    color = (255, 0, 0, 255)
+                    cv2.rectangle(img, min_loc, (min_loc[0] + w, min_loc[1] + h), color, 2)
+            for passive in os.listdir('backend\\static\\passives'):
+                templ = cv2.imread(f'backend\\static\\passives\\{passive}', cv2.IMREAD_UNCHANGED)
+                h, w = templ.shape[:-1]
+                alpha_channel = np.array(cv2.split(templ)[3])
+                result = cv2.matchTemplate(img2, templ, cv2.TM_SQDIFF_NORMED, mask=alpha_channel)
+                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+                if (min_val <= 0.1):
+                    print(passive)
+                    color = (0, 255, 0, 255)
+                    cv2.rectangle(img, min_loc, (min_loc[0] + w, min_loc[1] + h), color, 2)
+        else:
+            templ = cv2.imread('backend\\static\\passives\\Super_Hot_Watch.png', cv2.IMREAD_UNCHANGED)
+            h, w = templ.shape[:-1]
+            alpha_channel = np.array(cv2.split(templ)[3])
+            result = cv2.matchTemplate(img, templ, cv2.TM_SQDIFF_NORMED, mask=alpha_channel)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            color = (0, 255, 0, 255)
+            print(min_val)
+            cv2.rectangle(img, min_loc, (min_loc[0] + w, min_loc[1] + h), color, 2)
         status, buffer = cv2.imencode('.png', img)
         data = np.array(buffer)
         bytes = data.tobytes()
@@ -37,6 +71,7 @@ def analyze():
         temp.seek(0)
         return send_file(temp, download_name="temp.png")
     except Exception as e:
+        print(e)
         return str(e)
     
 if __name__ == "__main__":
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
-import React from "react";
-import Home from './components/Home';
+import "./App.css"
+import React from "react"
+import Home from "./components/Home"
 
 function App() {
-
-  return (
-    <Home />
-  );
+  return <Home />
 }
 
-export default App;
+export default App
