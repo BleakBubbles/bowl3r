562793f37fcc62bb5439a50d8254ee216c88c73f
Max Hua <max@einfolab.com>
2024-08-21 17:28:52 -0400

    Changed button colour and interpolation method when resizing

--- backend/server.py
+++ backend/server.py
@@ -21,12 +21,12 @@ def analyze():
         rawimg = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
         height = rawimg.shape[0]
         factor = 270/height
-        img = cv2.resize(rawimg, None, fx=factor, fy=factor)
-        img2 = img.copy()
+        img = cv2.resize(rawimg, None, fx=factor, fy=factor, interpolation=cv2.INTER_NEAREST_EXACT)
+        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         if all:
-            match(img, img2, 'actives', cv2.TM_SQDIFF_NORMED, 0.25, (0, 0, 255, 255))
-            match(img, img2, 'guns', cv2.TM_SQDIFF_NORMED, 0.25, (255, 0, 0, 255))
-            match(img, img2, 'passives', cv2.TM_SQDIFF_NORMED, 0.25, (0, 255, 0, 255))
+            match(img, img2, 'actives', cv2.TM_SQDIFF_NORMED, 0.2, (0, 0, 255, 255))
+            match(img, img2, 'guns', cv2.TM_SQDIFF_NORMED, 0.2, (255, 0, 0, 255))
+            match(img, img2, 'passives', cv2.TM_SQDIFF_NORMED, 0.2, (0, 255, 0, 255))
         status, buffer = cv2.imencode('.png', img)
         data = np.array(buffer)
         bytes = data.tobytes()
@@ -43,11 +43,12 @@ def match(img, img2, type, method, threshold, color):
                 templ = cv2.imread(f'backend\\static\\{type}\\{item}', cv2.IMREAD_UNCHANGED)
                 h, w = templ.shape[:-1]
                 alpha_channel = np.array(cv2.split(templ)[3])
-                result = cv2.matchTemplate(img2, cv2.cvtColor(templ, cv2.COLOR_BGRA2BGR), method, mask=alpha_channel)
+                result = cv2.matchTemplate(img2, cv2.cvtColor(templ, cv2.COLOR_BGRA2GRAY), method, mask=alpha_channel)
                 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
                 if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED] and min_val <= threshold:
                     cv2.rectangle(img, min_loc, (min_loc[0] + w, min_loc[1] + h), color, 1)
-                elif method in [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED] and cv2.max_val >= threshold:
+                    print(f"{item}: {min_val}")
+                elif method in [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED] and max_val >= threshold:
                     cv2.rectangle(img, max_loc, (max_loc[0] + w, max_loc[1] + h), color, 1)
 
 if __name__ == "__main__":
